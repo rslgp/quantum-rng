8050d216820a0b191a054af74069ced5389aaed8
rafael.pereira <rafael.pereira@medicalsys.com.br>
2025-03-06 21:30:20 -0300

    rate_limit_2h

--- quantum-rng/backend/social/routes/middleware/rate_limit.js
+++ quantum-rng/backend/social/routes/middleware/rate_limit.js
@@ -8,7 +8,7 @@ const RATE_LIMIT_PREMIUM = RATE_LIMIT_GLOBAL + 10; // Max requests allowed
 const KEY_RATE_LIMIT = `rate-limit:`;
 
 
-const WINDOW_TIME_GLOBAL = 8 * 3600; // Time window to reset limit in seconds (1 hour)
+const WINDOW_TIME_GLOBAL = 2 * 3600; // Time window to reset limit in seconds (1 hour)
 
 async function rateLimiter(req, res, next) {
     let userId = getUserId(req); //default use ip, config express to use req.ip (app.set('trust proxy', true);), other options req.headers['x-forwarded-for']?.join(',')[0]
